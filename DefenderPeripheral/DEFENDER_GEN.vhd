95ceda10f8e208f2031ae60ac1a9acc25904724f
sfazio3 <73261693+sfazio3@users.noreply.github.com>
2020-11-16 00:47:03 -0500

    New Architecture to decrease blank spaces

--- DefenderPeripheral/DEFENDER_GEN.vhd
+++ DefenderPeripheral/DEFENDER_GEN.vhd
@@ -25,12 +25,12 @@ architecture Internals of defense is   -- Define the internal architecture of th
 	signal init_lfsr0: std_logic_vector(14 downto 0);  -- random number seed
 	signal lfsr1     : std_logic_vector(14 downto 0);  -- another random number generator
 	signal init_lfsr1: std_logic_vector(14 downto 0);  -- other random numer seed
-	signal random 	  : std_logic_vector(14 downto 0);  -- overall random output
+	signal random 	  : std_logic_vector(2 downto 0);  -- overall random output
 	signal A 		  : std_logic_vector(0  downto 0); -- used for difficulty encoding LSB
 	signal B 		  : std_logic_vector(0  downto 0);
 	
 	-- States for Default, RightTwo, LeftTwo, and Sides
-	type state_type is (D, R2, L2, S);
+	type state_type is (N, L, R, M, L2, R2, S, N2);
 	signal state 	  : state_type;
 	signal defenders : std_logic_vector (6 downto 0);
 	type difficulty_type is ( easy, medium, hard, worst );
@@ -41,7 +41,7 @@ architecture Internals of defense is   -- Define the internal architecture of th
 	rand0 : process(cs, resetn) -- linear feedback shift register
 		begin
 		if resetn = '0' then
-			init_lfsr0 <= "011011001100010"; -- seed
+			init_lfsr0 <= "011011111111110"; -- seed
 		elsif rising_edge(cs) then
 			lfsr0 <= lfsr0(13 downto 0) & not(lfsr0(14) xor lfsr0(13)); -- randomizer
 			init_lfsr0 <= init_lfsr0 + 1; -- change random seed
@@ -58,115 +58,205 @@ architecture Internals of defense is   -- Define the internal architecture of th
 		end if;
 	end process;
 	
-	-- combining both shift registers in opposite directions for more randomness
-	random <= lfsr0 and lfsr1; 
+	-- combining both shift registers for more randomness
+	random <= ((lfsr0(12 downto 12) or lfsr1(1 downto 1)) & (lfsr0(7 downto 7) or lfsr1(3 downto 3)) & (lfsr0(5 downto 5) or lfsr1(9 downto 9))); 
 	
 
 	
 	StateMachine : process(cs, resetn) -- 
 	begin
 		if resetn = '0' then
-			state <= D;
+			state <= N;
 		elsif (rising_edge(cs)) then
 			case state is
-				
-				when D =>
-					--L
-					if 31700 < random(14 downto 0) AND random(14 downto 0) < 32767 then
-						defenders <= (A and B) & "001000";
-						if ((A > 0) and (B > 0)) then
+				when N =>
+					defenders <= "0000000";
+					case random(2 downto 0) is
+						when "000" =>
+							state <= R;
+						when "001" =>
+							state <= L;
+						when "010" =>
+							state <= R;
+						when "011" =>
+							state <= M;
+						when "100" =>
 							state <= L2;
-						end if;
-					end if;
-					--N
-					if 29700 < random(14 downto 0) AND random(14 downto 0) < 31700 then
-						defenders <= "0000000";
-					end if;
-					--S
-					if 26650 < random(14 downto 0) AND random(14 downto 0) < 29700 then
-						defenders <= ((not B) and A) & "00" & B & "00" & B;
-						state <= S;
-					end if;
-					--R2
-					if 24600 < random(14 downto 0) AND random(14 downto 0) < 26650 then
-						state <= R2;
-						defenders <= B & "00000" & (A or B);
-					end if;
-					--L2
-					if 22550 < random(14 downto 0) AND random(14 downto 0) < 24600 then
-						state <= L2;
-						defenders <= B & "00" & (A or B) & "000";
-					end if;
-					--M
-					if 20500 < random(14 downto 0) AND random(14 downto 0) < 22550 then
-						defenders <= (not(A and B)) & "00" & (A and B) & "00" & (A and B);
-					end if;
-					--N
-					if 19500 < random(14 downto 0) AND random(14 downto 0) < 20500 then
-						defenders <= "0000000";
-					end if;
-					--R
-					if 16400 < random(14 downto 0) AND random(14 downto 0) < 18450 then
-						defenders <= (A and B) & "000001";
-						if ((A > 0) and (B > 0)) then
+						when "101" =>
 							state <= R2;
-						end if;
-					end if;
-					--L
-					if 12800< random(14 downto 0) AND random(14 downto 0) < 16400 then
-						defenders <= (A and B) & "001000";
-						if ((A > 0) and (B > 0)) then
+						when "110" =>
+							state <= S;
+						when "111" =>
+							state <= S;
+					end case;
+				when L =>
+					defenders <= (A and B) & "001000";
+					case random(2 downto 0) is
+						when "000" =>
+							state <= L;
+						when "001" =>
+							if ((A(0 downto 0) = "1") and (B(0 downto 0) = "1")) then
+								state <= N2;
+							else
+								state <= R;
+							end if;
+						when "010" =>
+							state <= M;
+						when "011" =>
 							state <= L2;
-						end if;
-					end if;
-					--N
-					if 12300 < random(14 downto 0) AND random(14 downto 0) < 12800 then
-						defenders <= "0000000";
-					end if;
-					--S
-					if 10250 < random(14 downto 0) AND random(14 downto 0) < 12300 then
-						defenders <= ((not B) and A) & "00" & B & "00" & B;
-						state <= S;
-					end if;
-					--R2
-					if 8200 < random(14 downto 0) AND random(14 downto 0) < 10250 then
-						state <= R2;
-						defenders <= B & "00000" & (A or B);
-					end if;
-					--L2
-					if 6150 < random(14 downto 0) AND random(14 downto 0) < 8200 then
-						state <= L2;
-						defenders <= B & "00" & (A or B) & "000";
-					end if;
-					--M
-					if 3100 < random(14 downto 0) AND random(14 downto 0) < 6150 then
-						defenders <= (not(A and B)) & "00" & (A and B) & "00" & (A and B);
-					end if;
-					--N
-					if 1000 < random(14 downto 0) AND random(14 downto 0) < 3100 then
-						defenders <= "0000000";
-					end if;
-					--R
-					if 0 < random(14 downto 0) AND random(14 downto 0) < 1000 then
-						defenders <= (A and B) & "000001";
-						if ((A > 0) and (B > 0)) then
+						when "100" =>
+							if ((A(0 downto 0) = "1") and (B(0 downto 0) = "1")) then
+								state <= S;
+							else 
+								state <= R2;
+							end if;
+						when "101" =>
+							state <= S;
+						when "110" =>
+							state <= N2;
+						when "111" =>
+							state <= N;
+					end case;
+				when R =>
+					defenders <= (A and B) & "000001";
+					case random(2 downto 0) is
+						when "000" =>
+							state <= R;
+						when "001" =>
+							state <= M;
+						when "010" =>
+							if ((A(0 downto 0) = "1") and (B(0 downto 0) = "1")) then
+								state <= S;
+							else
+								state <= L2;
+							end if;
+						when "011" =>
 							state <= R2;
-						end if;
-					end if;
+						when "100" =>
+							state <= S;
+						when "101" =>
+							state <= N2;
+						when "110" =>
+							state <= N;
+						when "111" =>
+							if ((A(0 downto 0) = "1") and (B(0 downto 0) = "1")) then
+								state <= N;
+							else
+								state <= L;
+							end if;
+					end case;
+				when M =>
+					defenders <= (not(A and B)) & "00" & (A and B) & "00" & (A and B);
+					case random(2 downto 0) is
+						when "000" =>
+							state <= S;
+						when "001" =>
+							state <= L2;
+						when "010" =>
+							state <= R2;
+						when "011" =>
+							state <= S;
+						when "100" =>
+							state <= N2;
+						when "101" =>
+							state <= N;
+						when "110" =>
+							state <= L;
+						when "111" =>
+							state <= R;
+					end case;
 				when L2 =>
-					defenders <= "0000000";
-					state <= D;
+					defenders <= B & "00" & (A or B) & "000";
+					case random(2 downto 0) is
+						when "000" =>
+							state <= M;
+						when "001" =>
+							state <= S;
+						when "010" =>
+							state <= S;
+						when "011" =>
+							state <= N2;
+						when "100" =>
+							state <= N;
+						when "101" =>
+							state <= L;
+						when "110" =>
+							if ((A(0 downto 0) = "1") and (B(0 downto 0) = "1")) then
+								state <= M;
+							else 
+								state <= R;
+							end if;
+						when "111" =>
+							state <= M;
+					end case;
 				when R2 =>
-					defenders <= "0000000";
-					state <= D;
+					defenders <= B & "00000" & (A or B);
+					case random(2 downto 0) is
+						when "000" =>
+							state <= S;
+						when "001" =>
+							state <= N2;
+						when "010" =>
+							state <= N;
+						when "011" =>
+							if ((A(0 downto 0) = "1") and (B(0 downto 0) = "1")) then
+								state <= M;
+							else
+								state <= L;
+							end if;
+						when "100" =>
+							state <= R;
+						when "101" =>
+							state <= M;
+						when "110" =>
+							state <= M;
+						when "111" =>
+							state <= S;
+					end case;
 				when S =>
+					defenders <= ((not B) and A) & "00" & B & "00" & B;
+					case random(2 downto 0) is
+						when "000" =>
+							state <= M;
+						when "001" =>
+							state <= N2;
+						when "010" =>
+							state <= N;
+						when "011" =>
+							state <= L;
+						when "100" =>
+							state <= R;
+						when "101" =>
+							state <= M;
+						when "110" =>
+							state <= L2;
+						when "111" =>
+							state <= R;
+					end case;
+				when N2 =>
 					defenders <= "0000000";
-					state <= D;
-				when others =>
-					defenders <= "0000000";
+					case random(2 downto 0) is
+						when "000" =>
+							state <= L;
+						when "001" =>
+							state <= M;
+						when "010" =>
+							state <= L;
+						when "011" =>
+							state <= R;
+						when "100" =>
+							state <= M;
+						when "101" =>
+							state <= L2;
+						when "110" =>
+							state <= R2;
+						when "111" =>
+							state <= S;
+					end case;
 			end case;
 		end if;
 	end process;
+	
 	
 	DifficultyLatch : process (cs, resetn, IO_DATA)
 	begin 
